c6a1178d06a4562dedd4bb3adc05e4434623b39e
Lam L <ppoo44ttaa@gmail.com>
2021-03-14 23:09:46 +0300

    task brackets completed

--- src/index.js
+++ src/index.js
@@ -1,3 +1,33 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  const holder = []
+  const openBrackets = bracketsConfig.filter(elem => elem[0] !== elem[1]).map((elem) => {
+    return elem[0];
+  })
+  const closedBrackets = bracketsConfig.filter(elem => elem[0] !== elem[1]).map((elem) => {
+    return elem[1];
+  })
+  const sameBrackets = bracketsConfig.filter(elem => elem[0] === elem[1]).map(elem => elem[0])
+
+  for (let letter of str) { 
+      if(sameBrackets.includes(letter)) {
+        if(holder[holder.length - 1] === letter) {
+          holder.splice(-1,1)
+        } else {
+          holder.push(letter)
+        }
+      }
+      if(openBrackets.includes(letter)){ 
+          holder.push(letter)
+      }else if(closedBrackets.includes(letter)){ 
+          const openPair = openBrackets[closedBrackets.indexOf(letter)] 
+          if(holder[holder.length - 1] === openPair){ 
+              holder.splice(-1,1) 
+          }else{ 
+              holder.push(letter)
+              break 
+          }
+      }
+  }
+  return (holder.length === 0) 
+
 }
